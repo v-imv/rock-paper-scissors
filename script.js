08f4e932cf91347b483d843864149ae0dd2b0d45
v-imv <v.iminov@gmail.com>
2021-11-07 22:17:13 +0000

    Came up with my algorithm

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-// This function returns Rock / Paper / Scissors at random
-function computerPlay(){
-    let rock = 'Rock'
-    let paper = 'Paper'
-    let scissors = 'Scissors'
+// My Rock Paper Scissors Algorithm
+
+// Step 1. Prompt the player for their input:
+let askInput = window.prompt("Rock Paper or Scissors?")
+
+// Step 2. Make the player's input case insensitive and store it:
+let playerSelection = askInput.toLowerCase();
+
+// Step 3. Run a function to get input from the computer:
+let computerSelection = function computerPlay(){
+    let rock = 'rock'
+    let paper = 'paper'
+    let scissors = 'scissors'
     let x = Math.floor(Math.random() *101 )
 
     if(x <= 33.32){
@@ -13,3 +21,14 @@ function computerPlay(){
     }
     else {return(scissors)}
 }
+
+console.log(playerSelection);
+console.log(computerSelection());
+
+// Step 4. Store the computer's input:
+
+// Step 5. Determine the winner, player or computer?:
+
+// Step 6. Repeat steps 1 to 5 until either party wins 3 times (best of 5):
+
+// Step 7. Announce a winner:
