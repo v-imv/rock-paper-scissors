96ab8b16ffcd463993e7e154936fce45fe509536
v-imv <v.iminov@gmail.com>
2021-11-08 09:51:50 +0000

    Working Rock Paper Scissors Game

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // My Rock Paper Scissors Algorithm
+let playerScore = 0;
+let computerScore = 0;
+alert("Welcome To Rock Paper Scissors! Press 'OK' to begin a best of 5 game (first to 3 points).")
 
 // Step 1. Prompt the player for their input:
-let askInput = window.prompt("Rock Paper or Scissors?")
+while(playerScore < 3 || computerScore <3) {
+    if(playerScore === 3 || computerScore === 3){break;}
+    let askInput = window.prompt("Rock Paper or Scissors?")
 
 // Step 2. Make the player's input case insensitive and store it:
 let playerSelection = askInput.toLowerCase();
 
 // Step 3. Run a function to get input from the computer:
 // Step 4. Store the computer's input:
-let computerSelection = function computerPlay(){
+function computerPlay(){
     let rock = 'rock'
     let paper = 'paper'
     let scissors = 'scissors'
@@ -23,50 +28,59 @@ let computerSelection = function computerPlay(){
     else {return(scissors)}
 }
 
-console.log(playerSelection);
-console.log(computerSelection());
+let computerSelection = computerPlay();
 
 // Step 5. Determine the winner, player or computer and keep track of score:
-let playerScore = 0; // Potential infinite loop issue
-let computerScore = 0; // Potential infinite loop issue
-
 function winDeterminer(){
     if(playerSelection === 'rock' && computerSelection === 'rock'){
-        console.log("You both drew rock. It is a tie.");
+        alert("You both drew rock. It is a tie.");
     }
     else if(playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log("You lose.. Rock loses against Paper.");
+        alert("You lose.. Rock loses against Paper.");
         ++computerScore;
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        console.log("You win! Rock beats Scissors!");
+        alert("You win! Rock beats Scissors!");
         ++playerScore;
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log("You win! Papers beats Rock!");
+        alert("You win! Papers beats Rock!");
         ++playerScore;
     }
     else if(playerSelection === 'paper' && computerSelection === 'paper'){
-        console.log("You both drew paper. It is a tie.")
+        alert("You both drew paper. It is a tie.")
     }
     else if(playerSelection === 'paper' && computerSelection === 'scissors'){
-        console.log("You lose.. Paper loses against Scissors.");
+        alert("You lose.. Paper loses against Scissors.");
         ++computerScore;
     }
     else if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        console.log("You lose. Scissors lose against Rock.");
+        alert("You lose. Scissors lose against Rock.");
         ++computerScore;
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log("You win! Scissors beats Paper!");
+        alert("You win! Scissors beats Paper!");
         ++playerScore;
     }
     else if(playerScore === 'scissors' && computerSelection === 'scissors'){
-        console.log("You both drew scissors. It is a tie.");
+        alert("You both drew scissors. It is a tie.");
     }
     // else input from the player was incorrect and we must start again from step 1
 }
 
 // Step 6. Repeat steps 1 to 5 until either party wins 3 times (best of 5):
+// This step is a while loop at the top of the code in step 1
 
 // Step 7. Announce a winner:
+function winAnnouncer(){
+    if(playerScore === 3){
+        alert('Congratulations! You won with a score of ' + playerScore + ' to ' + computerScore + '.' + ' Refresh the page if you would like to play again.')
+    }
+    else if(computerScore === 3){
+        alert('Better luck next time.. You lost with a score of ' + playerScore + ' to ' + computerScore + '.' + ' Refresh the page if you would like to play again.')
+    }
+}
+
+winDeterminer();
+winAnnouncer();
+}
