b7a058a436dcb7b4c79cca1c437b68dd2d201b0e
v-imv <v.iminov@gmail.com>
2021-11-17 17:32:36 +0000

    rps selection bug

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+//alert("Welcome To Rock Paper Scissors! First to 5 points wins. \nPress 'OK' to begin a game.")
+
 const body = document.body;
 
 const rockButton = document.createElement('button');
@@ -5,121 +9,101 @@ body.appendChild(rockButton);
 rockButton.setAttribute('id', 'rock-button');
 rockButton.setAttribute('class', 'buttons');
 rockButton.textContent = 'Rock';
-rockButton.addEventListener('click', playerSelectionRock);
+rockButton.addEventListener('click', playerSelectionRockRound);
 
 const paperButton = document.createElement('button');
 body.appendChild(paperButton);
 paperButton.setAttribute('id', 'paper-button');
 paperButton.setAttribute('class', 'buttons');
 paperButton.textContent = 'Paper';
-paperButton.addEventListener('click', playerSelectionPaper);
+paperButton.addEventListener('click', playerSelectionPaperRound);
 
 const scissorsButton = document.createElement('button');
 body.appendChild(scissorsButton);
 scissorsButton.setAttribute('id', 'scissors-button');
 scissorsButton.setAttribute('class', 'buttons');
 scissorsButton.textContent = 'Scissors'
-scissorsButton.addEventListener('click', playerSelectionScissors);
+scissorsButton.addEventListener('click', playerSelectionScissorsRound);
 
-function playerSelectionRock(){
-    return("rock");
+let playerSelectRock = 'rock';
+function playerSelectionRockRound(){
+    playerSelectRock;
+    computerPlay();
+    winDeterminer();
+    winAnnouncer();
 }
 
-function playerSelectionPaper(){
-    return("paper");
+let playerSelectPaper = 'paper';
+function playerSelectionPaperRound(){
+    playerSelectPaper;
+    computerPlay();
+    winDeterminer();
+    winAnnouncer();
 }
 
-function playerSelectionScissors(){
-    return("scissors");
+let playerSelectScissors = 'scissors';
+function playerSelectionScissorsRound(){
+    playerSelectScissors;
+    computerPlay();
+    winDeterminer();
+    winAnnouncer();
 }
 
 
-// My Rock Paper Scissors Algorithm
-let playerScore = 0;
-let computerScore = 0;
-/* alert("Welcome To Rock Paper Scissors! First to 5 points wins. \nPress 'OK' to begin a game.")
-
-// Looping Steps 1 to 5 until there is a winner:
-while(playerScore < 3 || computerScore <3) {
-    if(playerScore === 3 || computerScore === 3){break;}
-    // Step 1. Prompt the player for their input:
-    let askInput = window.prompt("Rock Paper or Scissors?")
-
-// Step 2. Make the player's input case insensitive and store it:
-let playerSelection = askInput.toLowerCase();
-
-// Step 3. Run a function to get input from the computer:
-// Step 4. Store the computer's input:
 function computerPlay(){
-    let rock = 'rock'
-    let paper = 'paper'
-    let scissors = 'scissors'
     let x = Math.floor(Math.random() *101 )
 
     if(x <= 33.32){
-        return(rock)
+        return('rock')
     }
     else if(x >= 33.33 && x <= 66.66){
-        return(paper)
+        return('paper')
     }
-    else {return(scissors)}
+    else {return('scissors')}
 }
 
-let computerSelection = computerPlay();
-
-// Step 5. Determine the winner, player or computer and keep track of score:
 function winDeterminer(){
-    if(playerSelection === 'rock' && computerSelection === 'rock'){
+    if(playerSelectRock === 'rock' && computerPlay() === 'rock'){
         alert("You both drew rock. It is a tie.");
     }
-    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+    else if(playerSelectRock === 'rock' && computerPlay() === 'paper'){
         alert("You lose.. Rock loses against Paper.");
         ++computerScore;
     }
-    else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+    else if(playerSelectRock === 'rock' && computerPlay() === 'scissors'){
         alert("You win! Rock beats Scissors!");
         ++playerScore;
     }
-    else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        alert("You win! Papers beats Rock!");
+    else if(playerSelectPaper === 'paper' && computerPlay() === 'rock'){
+        alert("You win! Paper beats Rock!");
         ++playerScore;
     }
-    else if(playerSelection === 'paper' && computerSelection === 'paper'){
+    else if(playerSelectPaper === 'paper' && computerPlay() === 'paper'){
         alert("You both drew paper. It is a tie.")
     }
-    else if(playerSelection === 'paper' && computerSelection === 'scissors'){
+    else if(playerSelectPaper === 'paper' && computerPlay() === 'scissors'){
         alert("You lose.. Paper loses against Scissors.");
         ++computerScore;
     }
-    else if(playerSelection === 'scissors' && computerSelection === 'rock'){
+    else if(playerSelectScissors === 'scissors' && computerPlay() === 'rock'){
         alert("You lose. Scissors lose against Rock.");
         ++computerScore;
     }
-    else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+    else if(playerSelectScissors === 'scissors' && computerPlay() === 'paper'){
         alert("You win! Scissors beats Paper!");
         ++playerScore;
     }
-    else if(playerScore === 'scissors' && computerSelection === 'scissors'){
+    else if(playerSelectScissors === 'scissors' && computerPlay() === 'scissors'){
         alert("You both drew scissors. It is a tie.");
     }
-    else(alert("You shall not pass!! Until you input rock paper or scissors correctly that is..."))
 }
 
-// Step 6. Repeat steps 1 to 5 until either party wins 3 times (best of 5):
-// This step is a while loop at the top of the code in step 1
-
-// Step 7. Announce a winner:
 function winAnnouncer(){
-    if(playerScore === 3){
+    if(playerScore === 5){
         alert('Congratulations! You won with a score of ' + playerScore + ' to ' + computerScore + '.' + ' Refresh the page if you would like to play again.')
     }
-    else if(computerScore === 3){
+    else if(computerScore === 5){
         alert('Better luck next time.. You lost with a score of ' + playerScore + ' to ' + computerScore + '.' + ' Refresh the page if you would like to play again.')
     }
 }
 
-winDeterminer();
-winAnnouncer();
-}
-
-*/
